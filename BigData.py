f5f77387b1e3efbdd2e32dd75746fc96c1e81b0c
ianq1w1 <ianpereiraian2@gmail.com>
2023-10-18 00:50:11 -0300

    algumas adições sobre filtragem

--- BigData.py
+++ BigData.py
@@ -1,23 +1,40 @@
-
 from pyspark.sql import SparkSession
-import time 
+from pyspark.sql.functions import col
+import time
 
+# Iniciar a contagem de tempo
 start_time = time.time()
+
+# Criar a sessão Spark
 spark = SparkSession.builder \
-    .appName("Exemplo CSV PySpark") \
+    .appName("Big Data") \
     .getOrCreate()
-# Lendo um arquivo CSV e criando um DataFrame
-df = spark.read.csv("C:/Users/ianpe/Desktop/planilhateste.csv", header=True, inferSchema=True)
-#insira o caminho de algum arquivo CSV  para leitura 
 
-#faltam possiveis analises e tomadas de decisão em relação á o arquivo que for enviado aqui 
+# Ler o arquivo CSV e criar um DataFrame
+# Substitua o caminho do arquivo
+df = spark.read.csv("C:/Users/ianpe/Downloads/Crimes_-_2001_to_Present.csv", header=True, inferSchema=True)
+spark.conf.set("spark.sql.repl.eagerEval.enabled", "true")
 
 
-# Mostrar o esquema e as primeiras linhas do DataFrame
-df.printSchema()
-df.show()
+# Solicitar ao usuário o ano dos crimes
+ano = input("Digite o ano dos crimes que deseja visualizar: ")
+
+# Filtrar o DataFrame com base no ano inserido
+df_filtered = df.filter(col("Year") == int(ano))
+
+
+# Filtrando os crimes com descrição "THEFT"
+# depoooois df_filtered = df.filter(col("Description") == "THEFT")
+
+
+# Mostrar o esquema e as linhas do DataFrame filtrado
+df_filtered.printSchema()
+df_filtered.show()
+
+# Encerrar a contagem de tempo e calcular o tempo de execução
 end_time = time.time()
-execution_time =  end_time - start_time 
-print(execution_time)
-spark.stop()
+execution_time = end_time - start_time
+print("Tempo de execução:", execution_time)
 
+# Encerrar a sessão Spark
+spark.stop()
